e8265591a0889664b9684add9736c0bf66da510f
Neeshu Sharma <114111237+NeeshuSharmaa@users.noreply.github.com>
2023-12-29 23:04:22 +0530

    added link on social images & refactoring

--- src/frontend/components/footer/Footer.jsx
+++ src/frontend/components/footer/Footer.jsx
@@ -3,13 +3,21 @@ import "./Footer.css";
 export default function Footer() {
   return (
     <footer>
-      <small>
-        ©2023 Serenata Blooms | All Rights Reserved | Terms & Condition
-      </small>
+      <div>
+        <small>©2023 Serenata Blooms | All Rights Reserved </small>
+        <small>| Terms & Condition</small>
+      </div>
+
       <div className="socials">
-        <img src="/assets/icons/github.svg" alt="github" />
-        <img src="/assets/icons/linkedin.svg" alt="linkedin" />
-        <img src="/assets/icons/twitter.svg" alt="twitter" />
+        <a href="https://github.com/NeeshuSharmaa">
+          <img src="/assets/icons/github.svg" alt="github" />
+        </a>
+        <a href="https://www.linkedin.com/in/neeshusharmaa/">
+          <img src="/assets/icons/linkedin.svg" alt="linkedin" />
+        </a>
+        <a href="https://twitter.com/neeshusharmaa">
+          <img src="/assets/icons/twitter.svg" alt="twitter" />
+        </a>
       </div>
     </footer>
   );
