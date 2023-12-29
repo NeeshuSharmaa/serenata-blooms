89e111d1342930cc5b05400558707420be737551
Neeshu Sharma <114111237+NeeshuSharmaa@users.noreply.github.com>
2023-12-29 13:37:23 +0530

    style: navbar responsiveness

--- src/frontend/components/navbar/Navbar.jsx
+++ src/frontend/components/navbar/Navbar.jsx
@@ -7,20 +7,26 @@ import {
   faUser,
   faBars,
   faMagnifyingGlass,
+  faXmark,
+  faCaretUp,
 } from "@fortawesome/free-solid-svg-icons";
 import { Link, NavLink, useNavigate } from "react-router-dom";
 import { useFilterContext } from "../../contexts/FilterDataProvider";
+import { useEffect, useState } from "react";
 
 export default function Navbar() {
   const { filterState, dispatch } = useFilterContext();
+  const [menuActive, setMenuActive] = useState(false);
 
   const navigate = useNavigate();
+
   return (
     <nav>
       <Link to="/">
         {" "}
         <div className="logo-outer">
           <img src="/assets/images/nav-logo.png" alt="logo" />
+
           <div className="logo-inner">
             <h1>erenata Blooms</h1>
           </div>
@@ -38,23 +44,27 @@ export default function Navbar() {
         }}
       />
 
-      <div className="nav-links">
-        <FontAwesomeIcon icon={faMagnifyingGlass} className="search-icon" />
-        <NavLink to="/store">
-          <FontAwesomeIcon icon={faStore} className="font-aw-icons" />
-        </NavLink>
-        <NavLink to="/cart">
-          {" "}
-          <FontAwesomeIcon icon={faCartShopping} className="font-aw-icons" />
-        </NavLink>
-        <NavLink to="/wishlist">
-          <FontAwesomeIcon icon={faHeart} className="font-aw-icons" />
-        </NavLink>
-        <NavLink to="/profile">
-          {" "}
-          <FontAwesomeIcon icon={faUser} className="font-aw-icons" />
-          <FontAwesomeIcon icon={faBars} className="nav-menu" />
-        </NavLink>
+      <div style={{ position: "relative" }}>
+        <div className={menuActive ? "nav-links menu-active" : "nav-links"}>
+          <NavLink to="/store" className="nav-child-link">
+            <FontAwesomeIcon icon={faStore} className="font-aw-icons" />
+          </NavLink>
+          <NavLink to="/cart" className="nav-child-link">
+            <FontAwesomeIcon icon={faCartShopping} className="font-aw-icons" />
+          </NavLink>
+          <NavLink to="/wishlist" className="nav-child-link">
+            <FontAwesomeIcon icon={faHeart} className="font-aw-icons" />
+          </NavLink>
+          <NavLink to="/profile" className="nav-child-link">
+            <FontAwesomeIcon icon={faUser} className="font-aw-icons" />
+          </NavLink>
+        </div>
+        {menuActive && <FontAwesomeIcon icon={faCaretUp} className="caret" />}
+        <FontAwesomeIcon
+          icon={menuActive ? faXmark : faBars}
+          className="nav-menu"
+          onClick={() => setMenuActive((prev) => !prev)}
+        />
       </div>
     </nav>
   );
